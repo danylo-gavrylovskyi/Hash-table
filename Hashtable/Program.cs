84aab6ebd805a51d8bc8e9bb1beb0617d73f804e
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-03-27 13:12:19 +0300

    Created dictionary class

--- Hashtable/Program.cs
+++ Hashtable/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class StringsDictionary
+{
+    private const int InitialSize = 10;
+
+    private LinkedList[] _buckets = new LinkedList[InitialSize];
+
+    public void Add(string key, string value)
+    {
+
+    }
+
+    public void Remove(string key)
+    {
+
+    }
+
+    public string Get(string key)
+    {
+
+    }
+
+
+    private int CalculateHash(string key)
+    {
+        // function to convert string value to number 
+    }
+}
